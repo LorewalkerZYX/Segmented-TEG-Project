07ab5ac8f42b8e593d55b976c9a388dc85b4bc0e
LorewalkerZYX <101465715+LorewalkerZYX@users.noreply.github.com>
2024-06-21 14:10:06 +0100

    Add files via upload

---
+++ ANNTraining/Iterative Training/Bias4000/LoadDataQR1_.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def LoadData():
+    # preparing data
+    Train_X = pd.read_excel('QTrainX.xlsx')
+    Train_Y = pd.read_excel('QTrainY.xlsx')
+    Valid_X = pd.read_excel('QValidX.xlsx')
+    Valid_Y = pd.read_excel('QValidY.xlsx')
+    Test_X = pd.read_excel('QTestX.xlsx')
+    Test_Y = pd.read_excel('QTestY.xlsx')
+    TrainX = Train_X.iloc[:, :7]
+    TrainY = Train_Y.iloc[:, :1]
+    ValidX = Valid_X.iloc[:, :]
+    ValidY = Valid_Y.iloc[:, :]
+    TestX = Test_X.iloc[:, :]
+    TestY = Test_Y.iloc[:, :]
+
+    X_train = TrainX.to_numpy()
+    Y_train = TrainY.to_numpy()
+    X_Valid = ValidX.to_numpy()
+    Y_Valid = ValidY.to_numpy()
+    X_Test = TestX.to_numpy()
+    Y_Test = TestY.to_numpy()
+    return X_train, Y_train, X_Valid, Y_Valid, X_Test, Y_Test
